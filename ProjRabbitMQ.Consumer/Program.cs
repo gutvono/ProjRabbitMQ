08731110d94f03cb7d9979f99a6d4ec945197b4b
Gustavo Vono <102389423+gutvono@users.noreply.github.com>
2024-06-13 14:10:16 -0300

    estudos rabbitMq

---
+++ ProjRabbitMQ.Consumer/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using ProjRabbitMQ.Consumer.Services;
+using ProjRabbitMQ.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+const string QUEUE_NAME = "message";
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using (var connection = factory.CreateConnection())
+{
+    using (var channel = connection.CreateModel())
+    {
+        channel.QueueDeclare(queue: QUEUE_NAME,
+                      durable: false,
+                      exclusive: false,
+                      autoDelete: false,
+                      arguments: null);
+
+        while (true)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var returnMessage = Encoding.UTF8.GetString(body);
+                var message = JsonConvert.DeserializeObject<Message>(returnMessage);
+
+                Message msg = new MessageService().PostMessage(message).Result;
+                Console.WriteLine("Message: " + msg.Description);
+            };
+
+            channel.BasicConsume(queue: QUEUE_NAME,
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ ProjRabbitMQ.Consumer/Services/MessageService.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using ProjRabbitMQ.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjRabbitMQ.Consumer.Services
+{
+    public class MessageService
+    {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task<Message> PostMessage(Message msg)
+        {
+            try
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(msg), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await MessageService._httpClient.PostAsync("https://localhost:7154/api/Messages", content);
+                response.EnsureSuccessStatusCode();
+                string msgReturn = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<Message>(msgReturn);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
